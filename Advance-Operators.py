ce02b1dcbe26763f57601406e82cb8818f9e6aa0
Rana Hassnain <rhassnain353@gmail.com>
2026-09-04 17:05:29 +0500

    Add files via upload

---
+++ Advance-Operators.py
@@ -0,0 +1,15 @@
+#identity operators
+a = 5
+b = 4
+print(a is b)#False
+print(a is not b)#True
+#theory on my linked in account https://www.linkedin.com/in/hassnain-raza-381196431/
+
+#Membership Operators 
+a = "Hassnain Rana"
+print("H" in a)
+print("Ra" in a)
+
+b = [2,3,4,5,6,6,7,"Rana"]
+print("Rana" in b)
+print(9 in b)
