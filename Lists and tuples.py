4d44af3c816081ec57a8d9587b923418f437bf7e
Rana Hassnain <rhassnain353@gmail.com>
2026-09-06 10:34:32 +0500

    Add files via upload

---
+++ Lists and tuples.py
@@ -0,0 +1,44 @@
+#lists 
+list =[1,2,3,4,5,6,"Hassnain"]
+print(list)
+#Methods
+Fruits = ["Mango","Orange","Banana","Apple","pears","peaches"]
+print(Fruits[0])
+(Fruits.append("plums"))
+print(Fruits)
+(Fruits.remove("Mango"))
+print(Fruits)
+Fruits.sort()
+print(Fruits)
+print(len(Fruits))
+print(Fruits[0:5])
+print(Fruits[0:])
+print(Fruits[:5])
+print(Fruits[0:7:2])
+
+#lists 
+list =[1,2,3,4,5,6,"Hassnain"]
+print(list)
+#Methods
+Fruits = ["Mango","Orange","Banana","Apple","pears","peaches"]
+print(Fruits[0])
+(Fruits.append("plums"))
+print(Fruits)
+(Fruits.remove("Mango"))
+print(Fruits)
+Fruits.sort()
+print(Fruits)
+print(len(Fruits))
+print(Fruits[0:5])
+print(Fruits[0:])
+print(Fruits[:5])
+print(Fruits[0:7:2])
+
+#Tuple
+#a = (1) Type is int
+a = (1,2,2,3,4,5,6,7,8,"Hassnain Raza")#type is Tuple
+print(a)
+print(len(a))
+print(type(a))
+print(a.count(2))
+print(a[1:6])
