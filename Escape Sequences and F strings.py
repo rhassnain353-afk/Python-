a89f10ca59c21497e981dbf755afbe17a6a926a9
Rana Hassnain <rhassnain353@gmail.com>
2026-09-06 16:08:51 +0500

    Add files via upload

---
+++ Escape Sequences and F strings.py
@@ -0,0 +1,24 @@
+#Escape Sequences
+a = "hassnain raza"
+b = "Asad Riaz"
+print(a + "\n" + b)
+print(f"\'{a}\'")
+print(f"\"{b}\"")
+print(f"\\{a}\\")
+print(f"\t{b}")
+#F Strings
+name = input("Enter your name :")
+age = int(input("Enter your age:"))
+print(f"your Name is {name} and your age is {age}")
+
+#Comments 
+a = 7
+#Single line comment
+# 7 store in a
+#Comments are the statements that are not executed by the python interpreter.
+
+#Multi line comment
+a = "hassnain"
+""""
+This is a multi line comment
+"""
